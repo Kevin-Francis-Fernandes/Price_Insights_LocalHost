b2d196f7d4caf01dbc1260165bb6989e7474d28c
Kevin Fernandes <kevinfrancisfernandes@gmail.com>
2024-02-28 11:50:56 +0530

    stable 4

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -100,7 +100,7 @@ export async function scrapeAmazonProduct(url:string){
         lowestPrice: Number(currentPrice) || Number(originalPrice),
         highestPrice: Number(originalPrice) || Number(currentPrice),
         averagePrice: Number(currentPrice) || Number(originalPrice),
-        rating:Number(rating),
+        rating,
       sub_cat : "SmartPhones",
       main_cat : "Electronics",
         }
